e171a8df584e649dfaef090fa3d9889c6a61ba7f
José Viejo HUerta <Jviejo@users.noreply.github.com>
2024-04-05 19:25:28 +0200

    initial

---
+++ README.md
@@ -0,0 +1,26 @@
+# HTML LAB 1. lab de html
+## Objetivos
+1. Practicar con los elementos de html
+
+## Requerimientos.
+1. Fork este repositorio
+2. Clone este repositorio
+
+## Tareas a realizar
+0. Si no esta instalado bun, seguir instrucciones https://bun.sh/docs/installation
+1. Instalar dependencias
+ ```bash
+ bun install
+ ```
+2. Escribir en el fichero public/index.html
+3. Hacer bun test
+```bash
+bun test
+```
+4. La tarea estara acabada cuando todos los test esten pasados.
+
+
+
+
+
+
---
+++ html.test.js
@@ -0,0 +1,24 @@
+
+import { expect, test } from "bun:test";
+import express from "express"
+import path from "path"
+import { parse } from 'node-html-parser';
+async function setup() {
+    const app = express()
+    app.use(express.static(path.join(__dirname, 'public')))
+    app.listen(
+        3333
+    )
+    const response = await fetch("localhost:3333")
+    const html = await response.text()
+    return parse(html)
+}
+
+test("id1 existe", async () => {
+    // arrange
+    const parsed = await setup()
+    // act
+    const elemento = parsed.getElementById("id1")
+    // assert
+    expect(elemento).not.toBeNull();
+});
