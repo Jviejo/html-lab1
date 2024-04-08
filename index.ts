698e34631e6695aa22f14f1c50a4c953cf5aad3a
Carlosn04 <c.navajas4@gmail.com>
2024-04-08 11:05:32 +0200

    "cors added for the test"

--- index.ts
+++ index.ts
@@ -1,11 +1,13 @@
 
 import express from "express"
 import path from "path"
+import cors from "cors"
 
 const app = express()
+app.use(cors())
 
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.listen(
-    3333
-)
+app.listen(3333, () => {
+    console.log("Server running on port 3333");
+});
